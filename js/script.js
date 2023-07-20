51aa83f10d5e1030f04a807b24230f1ad4052323
Anton Petrovich <63902821+antonpetrovich13@users.noreply.github.com>
2023-07-20 17:30:26 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 const input = document.querySelector('.calculator__input');
-const result = document.querySelector('.eq');
 const deleteAll = document.querySelector('.delAll');
 const deleteLastValue = document.querySelector('.del');
+let firstValue = '';
+let secondValue = '';
+let operation = '';
+let result = false;
 
 
 document.addEventListener('click', function (e) {
@@ -12,22 +15,102 @@ document.addEventListener('click', function (e) {
 
 	let value = e.target.innerHTML;
 
-	function addValue() {
-		if (e.target.className.includes('delAll') || e.target.className.includes('del')) {
-			return;
+	// Расчёт результата по шаблону при нажатии на кнопку "РАВНО"
+	if (e.target.className.includes('eq')) {
+		result = true;
+		let x = +firstValue;
+		let y = +secondValue;
+		let op = operation;
+
+		//ОБНУЛЕНИЕ
+		operation = '';
+		secondValue = '';
+		result = false;
+
+		//ПОЛУЧЕНИЕ РЕЗУЛЬТАТА
+		switch (op) {
+			case '+':
+				return firstValue = input.value = x + y;
+			case '-':
+				return firstValue = input.value = x - y;
+			case '*':
+				return firstValue = input.value = x * y;
+			case '/':
+				return firstValue = input.value = x / y;
+			case '^':
+				return firstValue = input.value = Math.pow(x, y)
 		}
-		input.value += value;
 	}
 
-	addValue();
+	// ДОБАВЛЕНИЕ ЗНАЧЕНИЙ В ПОЛЕ
+	function addValue() {
+		if (e.target.className.includes('delAll') || e.target.className.includes('del') || e.target.className.includes('eq')) {
+			return;
+		}
 
+		if (e.target.className.includes('calculator__button_num')) {
+
+			// ЗАПОЛНЕНИЕ ЗНАЧЕНИЯ ДЛЯ ПЕРВОГО ЧИСЛА
+			if (secondValue === '' && operation === '') {
+				firstValue += value;
+
+				// УСТРАНЕННИЕ ЗНАЧЕНИЙ ТИПА "02"
+				if (firstValue.startsWith(0) && !firstValue.includes('.')) {
+					firstValue = firstValue.split('').splice(1).join('')
+				}
+				input.value = firstValue;
+				// ЗАПОЛНЕНИЕ ЗНАЧЕНИЯ ДЛЯ ВТОРОГО ЧИСЛА
+			} else if (firstValue !== '' && operation !== '') {
+				secondValue += value;
+				input.value = `${firstValue} ${operation} ${secondValue}`;
+			}
+
+			// ДЕЙСТВИЯ ПРИ НАЖАТИИ НА СИСТЕМНЫЕ КНОПКИ
+		} else if (e.target.className.includes('calculator__button_sys')) {
+
+			// ЗАПОЛНЕНИЕ ПЕРЕМЕННОЙ OPERATION
+			if (firstValue !== '' && operation === '') {
+				operation += value;
+				input.value = `${firstValue} ${operation}`;
+			} // НЕ ДАЕМ ВВЕСТИ 2 МАТЕМАТИЧЕСКИХ ЗНАКА ПОДРЯД
+			else if (firstValue !== '' && operation !== '' && secondValue === '') {
+				return;
+			} // ОСТАНАВЛИВАЕМ ВВОД, ЕСЛИ ЗАПОЛНЕНЫ ВСЕ ПЕРЕМЕННЫЕ, НО РЕЗУЛЬТАТ ВЫЧИСЛЕН НЕ БЫЛ
+			else if (firstValue !== '' && operation !== '' && secondValue !== '' && result === false) {
+				return
+			}
+		}
+	}
+	addValue();
 })
 
+
 deleteAll.onclick = function () {
-	input.value = '';
+	firstValue = '';
+	operation = '';
+	secondValue = '';
+	input.value = 0;
 }
 
 deleteLastValue.onclick = function () {
+
+	// ЕСЛИ ПЕРВОЕ ЧИСЛО ПРЕДСТАВЛЯЕТ СОБОЙ ЦИФРУ, ТО ПРИ ЕГО УДАЛЕНИИ ВЕРНУТЬ 0
+	if ((firstValue > 0 && firstValue < 10) && operation === '') {
+		firstValue = '';
+		return input.value = 0;
+	}
+
+	// ИСПРАВЛЕНИЕ ПЕРВОГО ЧИСЛА
+	if (operation === '') {
+		// firstValue = firstValue.toString();
+		firstValue = firstValue.toString().split('').reverse().splice(1).reverse().join('');
+		return input.value = firstValue;
+	} // ИСПРАВЛЕНИЕ МАТЕМАТИЧЕСКОГО ЗНАКА
+	else if (operation !== '' && secondValue === '') {
+		operation = '';
+	} else if (operation !== '' && secondValue !== '') {
+		secondValue = secondValue.split('').reverse().splice(1).reverse().join('');
+	}
 
 	let str = input.value;
 
@@ -40,3 +123,4 @@ deleteLastValue.onclick = function () {
 input.onfocus = function () {
 	return input.blur();
 }
+
